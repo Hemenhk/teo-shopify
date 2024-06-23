0fe9da109ffd3f335a04e462735ddab8d9e5d90f
Hemenhk <hemen.hiwakamal@hotmail.com>
2024-06-24 01:36:01 +0200

    change env

--- src/shopify-client.ts
+++ src/shopify-client.ts
@@ -3,5 +3,5 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 export const client = createStorefrontApiClient({
   storeDomain: process.env.NEXT_PUBLIC_STORE_DOMAIN,
   apiVersion: "2024-04",
-  publicAccessToken: process.env.NEXT_PUBLIC_STOREFRONT_PUBLIC_KEY,
+  publicAccessToken: process.env.NEXT_PUBLIC_STOREFRONT_PK,
 });
